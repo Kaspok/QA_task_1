6b6890027d63a86b50a91c4bf979946a4ef59b34
Kaspok <kaspok251198uU@yandex.ru>
2021-07-03 16:50:32 +0300

    Исправление некорректных проверок после заполнения формы.

--- src/test/java/com/demoqa/StudentRegistrationForm.java
+++ src/test/java/com/demoqa/StudentRegistrationForm.java
@@ -2,12 +2,11 @@ package com.demoqa;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Condition.*;
 import static  com.codeborne.selenide.Selenide. * ;
 
@@ -28,23 +27,29 @@ public class StudentRegistrationForm {
         $("#firstName").setValue("Lera");
         $("#lastName").setValue("Ivanova");
         $("#userEmail").setValue("Ivanova@example.com");
-        $(new Selectors.ByText("Female")).click();
+        $(byText("Female")).click();
         $("#userNumber").setValue("5674387181");
+
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue("1");
         $(".react-datepicker__year-select").selectOptionByValue("1998");
         $(".react-datepicker__day--020").click();
+
         SelenideElement Subject = $("#subjectsInput");
         Subject.setValue("Math").pressEnter();
         Subject.setValue("Histor").pressEnter();
-        $(new Selectors.ByText("Reading")).click();
-        $(new Selectors.ByText("Music")).click();
+
+        $(byText("Reading")).click();
+        $(byText("Music")).click();
+
         $("#uploadPicture").sendKeys("C:\\Users\\79051\\Desktop\\ava.jpg");
         $("#currentAddress").setValue("This is Current Address");
+
         $("#react-select-4-input").shouldBe(Condition.disabled); // Проверям, что поле City не доступно для редактирования (так как не заполнено поле State).
         $("#react-select-3-input").setValue("Uttar").pressEnter(); // Заполняем поле State.
         $("#react-select-4-input").shouldBe(Condition.enabled).setValue("Me").pressEnter(); // Проверяем что поле City стала доступна для редактирования, вводим данные.
-        executeJavaScript("window.scrollTo(0, 200)");
+
+        executeJavaScript("window.scrollTo(0, 200)"); // Прокручиваем страницу вниз
         $("#submit").click();
 
         // Проверка таблицы и веденных данных
@@ -52,35 +57,16 @@ public class StudentRegistrationForm {
         $x("//thead/tr/th[1]").shouldHave(text("Label"));
         $x("//thead/tr/th[2]").shouldHave(text("Values"));
 
-        $x("//tbody/tr[1]/td[1]").shouldHave(text("Student Name"));
-        $x("//tbody/tr[1]/td[2]").shouldHave(text("Lera Ivanova"));
-
-        $x("//tbody/tr[2]/td[1]").shouldHave(text("Student Email"));
-        $x("//tbody/tr[2]/td[2]").shouldHave(text("Ivanova@example.com"));
-
-        $x("//tbody/tr[3]/td[1]").shouldHave(text("Gender"));
-        $x("//tbody/tr[3]/td[2]").shouldHave(text("Female"));
-
-        $x("//tbody/tr[4]/td[1]").shouldHave(text("Mobile"));
-        $x("//tbody/tr[4]/td[2]").shouldHave(text("5674387181"));
-
-        $x("//tbody/tr[5]/td[1]").shouldHave(text("Date of Birth"));
-        $x("//tbody/tr[5]/td[2]").shouldHave(text("20 February,1998"));
-
-        $x("//tbody/tr[6]/td[1]").shouldHave(text("Subjects"));
-        $x("//tbody/tr[6]/td[2]").shouldHave(text("Maths, History"));
-
-        $x("//tbody/tr[7]/td[1]").shouldHave(text("Hobbies"));
-        $x("//tbody/tr[7]/td[2]").shouldHave(text("Reading, Music"));
-
-        $x("//tbody/tr[8]/td[1]").shouldHave(text("Picture"));
-        $x("//tbody/tr[8]/td[2]").shouldHave(text("ava.jpg"));
-
-        $x("//tbody/tr[9]/td[1]").shouldHave(text("Address"));
-        $x("//tbody/tr[9]/td[2]").shouldHave(text("This is Current Address"));
-
-        $x("//tbody/tr[10]/td[1]").shouldHave(text("State and City"));
-        $x("//tbody/tr[10]/td[2]").shouldHave(text("Uttar Pradesh Merrut"));
+        $x("//td[text()='Student Name']/following::td").shouldHave(text("Lera Ivanova"));
+        $x("//td[text()='Student Email']/following::td").shouldHave(text("Ivanova@example.com"));
+        $x("//td[text()='Gender']/following::td").shouldHave(text("Female"));
+        $x("//td[text()='Mobile']/following::td").shouldHave(text("5674387181"));
+        $x("//td[text()='Date of Birth']/following::td").shouldHave(text("20 February,1998"));
+        $x("//td[text()='Subjects']/following::td").shouldHave(text("Maths, History"));
+        $x("//td[text()='Hobbies']/following::td").shouldHave(text("Reading, Music"));
+        $x("//td[text()='Picture']/following::td").shouldHave(text("ava.jpg"));
+        $x("//td[text()='Address']/following::td").shouldHave(text("This is Current Address"));
+        $x("//td[text()='State and City']/following::td").shouldHave(text("Uttar Pradesh Merrut"));
 
         sleep(1000);
 
